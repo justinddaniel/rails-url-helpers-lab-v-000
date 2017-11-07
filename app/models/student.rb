f57c2f4422cad47fd044859560f957aa66e47c46
Justin Daniel <justinddaniel@gmail.com>
2017-11-07 23:10:39 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ActiveRecord::Base
 
-  after initialize :init 
+  after initialize :init
 
-  def init 
-    self.active = true if self.active.nil?
+  def init
+    self.active = true if (self.has_attribute? :active) && self.active.nil?
   end
-  
+
   def to_s
     self.first_name + " " + self.last_name
   end
