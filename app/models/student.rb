270c582916309107ab6e3bb53ed5264dda71e531
Justin Daniel <justinddaniel@gmail.com>
2017-11-07 23:07:11 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ActiveRecord::Base
+
+  after initialize :init 
+
+  def init 
+    self.active = true if self.active.nil?
+  end
+  
   def to_s
     self.first_name + " " + self.last_name
   end
